7f606a1330b643f60cb31e9fb7a78fa4762bcd7e
IsvDenn <81180103+IsvDenn@users.noreply.github.com>
2022-11-14 22:16:57 +0200

    Adding react native navigation

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { mainpage } from "./src/mainpage/mainpage";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <mainpage></mainpage>
   );
 }
 
