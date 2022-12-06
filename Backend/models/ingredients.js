856f644711c837cfdec4c7baf8cd51bcba89c778
Alexandru Melcescu <melcescu1997@gmail.com>
2022-12-06 11:18:40 -0800

    Continuation of Backend

--- Backend/models/ingredients.js
+++ Backend/models/ingredients.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose")
 
+//schema for the ingredients that includes:
+//the name, quantity, and the QR codes that some of 
+//the fruits and vegetables have on their product stickers
 const schema = new mongoose.Schema({
     name: {
         type: String,
